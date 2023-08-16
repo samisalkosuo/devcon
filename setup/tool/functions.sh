592ecb84dab2ceac9f324ee30a9d2336457f3ff5
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2023-08-16 14:08:46 +0300

    added python

--- setup/tool/functions.sh
+++ setup/tool/functions.sh
@@ -58,3 +58,34 @@ function install-java
     #alternatives --config java
 
 }
+
+function install-python
+{
+    #install python from source
+
+    local PYTHON_VERSION=$1
+
+    INSTALL_DIR=python-install
+    mkdir -p $INSTALL_DIR
+    cdir=$(pwd)
+    cd $INSTALL_DIR
+
+    #remove existing python3 
+    #dnf -y remove python3  || true
+
+    wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+
+    tar -xf Python-$PYTHON_VERSION.tgz
+
+    cd Python-$PYTHON_VERSION
+
+    #configuration options: https://docs.python.org/3/using/configure.html
+    sudo ./configure --prefix=/usr/local --enable-loadable-sqlite-extensions --enable-optimizations --with-lto --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
+    sudo make
+    sudo make install
+
+    #remove install dir
+    cd $cdir
+    sudo rm -rf $INSTALL_DIR
+
+}
