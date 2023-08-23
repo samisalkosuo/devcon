5e4a61b38fb3321146fb81c53b113164359b4f61
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2023-08-23 12:23:24 +0300

    added uid

--- setup/base/create-user.sh
+++ setup/base/create-user.sh
@@ -1,5 +1,8 @@
 
 
-useradd $DEVCON_USER -d $DEVCON_USER_HOME -G wheel
+USER_ID=24816
+groupadd -g $USER_ID $DEVCON_USER
+
+useradd $DEVCON_USER -d $DEVCON_USER_HOME -G wheel -g $USER_ID -u $USER_ID
 
 echo "$DEVCON_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
