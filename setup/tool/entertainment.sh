2535fb647a7b17d1229036c40ab8a9910b3bc5b8
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2023-08-24 15:51:35 +0300

    added 2048

--- setup/tool/entertainment.sh
+++ setup/tool/entertainment.sh
@@ -40,3 +40,24 @@ function install-cmatrix
     cd $cdir
     sudo rm -rf $INSTALL_DIR
 }
+
+function install-2048
+{
+    local cdir=$(pwd)
+    local INSTALL_DIR=${cdir}/misc
+
+    mkdir -p $INSTALL_DIR
+    cd $INSTALL_DIR
+
+
+    #2048 cli
+    #https://github.com/tiehuis/2048-cli
+    git clone https://github.com/tiehuis/2048-cli
+    cd 2048-cli
+    make
+    sudo mv 2048 /usr/local/bin/
+
+
+    cd $cdir
+    sudo rm -rf $INSTALL_DIR
+}
