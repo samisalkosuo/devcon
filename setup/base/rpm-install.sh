f305193636df682caf1ecbfd7dc38444726c7592
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2023-08-26 16:12:42 +0300

    added comment

--- setup/base/rpm-install.sh
+++ setup/base/rpm-install.sh
@@ -1,4 +1,6 @@
 #rpm installs
 
+#EPEL reositories
+#check https://dl.fedoraproject.org/pub/epel/ and download latest file when needed
 dnf -y install rpm/epel-release-latest-9.noarch.rpm 
 dnf -y install rpm/epel-next-release-latest-9.noarch.rpm
