ccb0188949cbbb6f85682a2c601874a1b0b20bef
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2023-10-20 15:30:21 +0300

    added postgresql client

--- setup/tool/functions.sh
+++ setup/tool/functions.sh
@@ -166,3 +166,18 @@ EOF'
     sudo dnf -y install mongodb-mongosh
     
 }
+
+function install-postgresql-client
+{
+    local VERSION=$1
+
+    # Install the repository RPM:
+    sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+    # Disable the built-in PostgreSQL module:
+    sudo dnf -qy module disable postgresql
+
+    # Install PostgreSQL:
+    sudo dnf install -y postgresql$VERSION
+
+}
